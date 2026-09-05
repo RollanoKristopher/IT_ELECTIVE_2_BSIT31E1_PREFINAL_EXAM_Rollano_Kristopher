fc6356ad32c5a657a2dd32373e76b28cd7d23561
Kristopher Rj Rollano <kristopherrjrollano@gmail.com>
2026-09-05 22:18:14 +0800

    feat: add ExamQuestion model

---
+++ Models/ExamQuestion.cs
@@ -0,0 +1,16 @@
+﻿namespace ExamMVC.Models
+{
+    public class ExamQuestion
+    {
+
+        public int Number { get; set; }
+        public string QuestionText { get; set; } = "";
+        public string? CodeSnippet { get; set; }
+        public Dictionary<string, string> Choices { get; set; } = new();
+        public string CorrectLetter { get; set; } = "";
+        public string CorrectAnswerText => Choices.TryGetValue(CorrectLetter, out var v) ? v : "";
+    }
+} 
+
+
+ 

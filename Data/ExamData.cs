04f7c235e18768bc823cbd8e612f19c320c53d7e
Kristopher Rj Rollano <kristopherrjrollano@gmail.com>
2026-09-05 22:47:33 +0800

    feat: answer Q1 - database persistence vs in-memory

--- Data/ExamData.cs
+++ Data/ExamData.cs
@@ -6,7 +6,19 @@ namespace ExamMVC.Data
     {
         public static List<ExamQuestion> Questions { get; } = new()
         {
-          
+            new ExamQuestion
+            {
+                Number = 1,
+                QuestionText = "What is the main problem solved by using a database instead of an in-memory collection?",
+                Choices = new Dictionary<string,string>
+                {
+                    {"A","It makes C# code shorter"},
+                    {"B","It prevents the application from restarting"},
+                    {"C","It allows data to persist after the application stops"},
+                    {"D","It removes the need for MVC"}
+                },
+                CorrectLetter = "C"
+            }
         };
     }
 }

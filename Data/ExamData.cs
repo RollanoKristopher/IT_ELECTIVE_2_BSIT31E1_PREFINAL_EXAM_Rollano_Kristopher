1eae1f406147f2003853b95d06e40677e3cb4e92
Kristopher Rj Rollano <kristopherrjrollano@gmail.com>
2026-09-05 22:40:19 +0800

    feat: add empty ExamData store

---
+++ Data/ExamData.cs
@@ -0,0 +1,12 @@
+﻿using ExamMVC.Models;
+
+namespace ExamMVC.Data
+{
+    public static class ExamData
+    {
+        public static List<ExamQuestion> Questions { get; } = new()
+        {
+          
+        };
+    }
+}
